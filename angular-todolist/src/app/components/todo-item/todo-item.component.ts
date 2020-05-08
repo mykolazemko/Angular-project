1901e91edf5c95870a80b164684216e69e61cc4b
MKL <zmkmkl@gmail.com>
2020-05-08 18:42:02 +0300

    TodoLtlist finished

--- angular-todolist/src/app/components/todo-item/todo-item.component.ts
+++ angular-todolist/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
 import { Todo } from 'src/app/models/Todo';
 
 @Component({
@@ -8,8 +9,9 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
   }
@@ -25,11 +27,15 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo){
+    // Toggle in UI
     todo.completed = !todo.completed;
+    //Togle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => 
+      console.log(todo));
   }
 
   onDelete(todo){
-    console.log('delete')
+    this.deleteTodo.emit(todo);
   }
 
 }
